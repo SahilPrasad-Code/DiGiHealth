6f787679a2006866b695c4233fc73f58b40ba44b
Sahil <sahilp6354@gmail.com>
2021-09-16 10:24:12 +0530

    day3 complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Center(
-        child: Text("Login Page"),
+        child: Text("Login Page",
+            style: TextStyle(
+                fontSize: 40, 
+                color: Colors.blue, 
+                fontWeight: FontWeight.bold
+            ),
         ),
+      ),
     );
   }
 }
